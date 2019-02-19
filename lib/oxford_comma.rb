d795ca0ede8b4ed83a8d3ed739f62a214e350e66
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 18:18:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,10 +8,10 @@ def oxford_comma(array)
     newArray = array.insert(1, ", ").insert(3, ", ").insert(4, "and ")
     return newArray.join
   elsif array.length > 3
-    position_for_and = array.length - 1 
-    newArray = array.insert(position_for_and, "and ")
-    joinedNewArray = newArray.join(", ") 
-    return joinedNewArray
+    newArray = []
+    array.each do |element|
+      
+    
   end 
 end
 
