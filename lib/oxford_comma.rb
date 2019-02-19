383465394377015fadb370bbe347acd9943aaeea
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 17:45:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,6 @@ def oxford_comma(array)
   if array.length == 1 
     return array.join
   elsif array.length == 2 
-    newArray = array.insert(1 " and ")
-    return newArray 
+    return array
   end 
 end
